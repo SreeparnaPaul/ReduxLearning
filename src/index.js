8037c632dbc7fee4c0920ff68820527ebd220929
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-04-04 21:46:18 +0530

    #action,actionTypes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import { addTask, removeTask } from "./action";
 import store from "./store";
-console.log(store);
+
+
+store.dispatch(addTask("Task 1"))
+console.log(store.getState());
+
+store.dispatch(removeTask(1))
+console.log(store.getState());
