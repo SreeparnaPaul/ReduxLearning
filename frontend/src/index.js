84f9a500c870bedd5cbcf7fab7c5fc3a1746c052
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-04-05 19:42:59 +0530

    #custom middleware for api call

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,37 @@
-// import { getTasks } from "./store/tasksReduxToolkit";
+ import { getTasks } from "./store/tasksReduxToolkit";
 import store from "./store/configureStore";
 import axios from 'axios'
 import { fetchTasks } from "./store/tasksReduxToolkit";
 
 //1.simple function method
-// const gettingTasks = async() =>{
-//     try {
-//        //calling an api
-//     const response =await axios.get("http://localhost:5000/api/tasks");
-//     console.log(response);
-//     //dispatch action
-//     store.dispatch(getTasks({tasks: response.data})) ;
-//     } catch (error) {
-//         store.dispatch({
-//             type:"SHOW_ERROR",
-//             payload:{error:error.message},
-//         })
-//     }
+const gettingTasks = async() =>{
+    try {
+       //calling an api
+    const response =await axios.get("http://localhost:5000/api/tasks");
+    console.log(response);
+    //dispatch action
+    store.dispatch(getTasks({tasks: response.data})) ;
+    } catch (error) {
+        store.dispatch({
+            type:"SHOW_ERROR",
+            payload:{error:error.message},
+        })
+    }
     
-// }
+}
 
-// gettingTasks();
+gettingTasks();
 
-store.dispatch(fetchTasks());
+//2.createAsyncThunk()
+
+// store.dispatch(fetchTasks());
+
+store.dispatch({
+    type:"apiRequest",
+    payload:{
+        url:"/tasks",
+        onSuccess:"tasks/getTasks",
+        onError:"SHOW_ERROR"
+    }
+})
 
--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,15 +1,14 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import employeeReducer from './employees';
 import taskReducer from './tasksReduxToolkit';
-import logger from 'redux-logger';
 import error from '../middleware/error';
-
+import api from '../middleware/api';
 const store = configureStore({
     reducer:{
     tasks:taskReducer,
     employees:employeeReducer
     },
-    middleware:(getDefaultMiddleware)=>[...getDefaultMiddleware(),logger,error],
+    middleware:(getDefaultMiddleware)=>[...getDefaultMiddleware(),api,error],
 })
 
 export default store;
