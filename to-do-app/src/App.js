57fe168a5677948938d0694360d0d54be85f03a5
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-04-06 13:51:18 +0530

    #addTask

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 const App = () => {
   return (
   
       <div>
+      <AddTask/>
         <Tasks />
       </div>
 
